c592b187b9c9cb4e0732eb7bc6c6c98c7e484cfe
Kavya Ganji <kavyaganji1901@gmail.com>
2019-12-24 19:34:04 +0530

    Completed first stage of resume

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+app.use(express.static('public'))
+app.get('/', (req, res) => res.sendFile(__dirname+'/public/html/resume.html'))
 
-app.get('/', (req, res) => res.sendFile(__dirname+'/public/html/resume.html))
-
-app.listen(port, () => console.log(`Example app listening on pornot ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
